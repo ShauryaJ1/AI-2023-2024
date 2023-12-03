6359ee080196f157545b77b477f48434bc32f661
ShauryaJ1 <shaurya.s.jain@gmail.com>
2023-12-03 17:12:03 -0500

    othello find stuff

--- othello_1.py
+++ othello_1.py
@@ -11,8 +11,29 @@ def makeMove(boardString):
    return ''
 
 def determineMoves(boardString,tokenToPlay):
-   return ''
+   opposite = 'x' if tokenToPlay == 'o' else 'x'
+   board_list = list(boardString)
+   possible_indices = [i for i,itm in enumerate(board_list) if itm=='.']
+   moves= []
+   for idx in possible_indices:
+      directions_to_check = []
+      for direction in directions:
+            if direction[0] + idx + direction[1]*8<64 and board_list[direction[0] + idx + direction[1]*8]==opposite:
+               directions_to_check.append(direction)
+      for direction in directions_to_check:
+         psblIdx = idx
+         while psblIdx<64:
+            psblIdx+=8
+            if board_list[psblIdx] == opposite:
+               continue
+            elif(board_list[psblIdx]==tokenToPlay):
+               moves.append(psblIdx)
+               break
+            else:
+               break
+               
 
+   return moves,boardString
 
 if __name__ == '__main__':
     if len(args)==0:
@@ -29,14 +50,15 @@ if __name__ == '__main__':
          else:
             tokenToPlay = 'o'
     else:
-       board = args[0]
-       tokenToPlay = args[1]
+       board = args[0].lower()
+       tokenToPlay = args[1].lower()
+    directions = [(0, 1), (1, 0), (0, -1), (-1, 0), (1, 1), (-1, -1), (1, -1), (-1, 1)]
     board = board.lower()
-    printBoard(board)
+    moves,boardUpdated = determineMoves(board,tokenToPlay)
+    printBoard(boardUpdated)
     print('\n')
     print1DREP(board)
-    print(determineMoves(board,tokenToPlay),'\n')
-
+    print(moves,'\n')
 
 '''
 testing boards:
