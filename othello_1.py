fc37110807e76ced81e111fa38851e5505112f26
ShauryaJ1 <shaurya.s.jain@gmail.com>
2023-12-03 20:17:45 -0500

    works

--- othello_1.py
+++ othello_1.py
@@ -20,27 +20,41 @@ def determineMoves(boardString,tokenToPlay):
    for idx in possible_indices:
       directions_to_check = []
       for direction in directions:
-            if direction[1] + idx + direction[0]*8<64 and board_list[direction[1] + idx + direction[0]*8]==opposite:
+            
+            if 0<=(direction[1] + idx + direction[0]*8)<64 and board_list[direction[1] + idx + direction[0]*8]==opposite:
             #    print(idx, direction[1] + idx + direction[0]*8)
                directions_to_check.append(direction)
     #   if directions_to_check:
     #      print(directions_to_check)
       for direction in directions_to_check:
+         prevIdx = idx
          psblIdx = idx
         #  print(direction)
+         
          while psblIdx<64:
-
+            prevIdx = psblIdx
             psblIdx+=8*direction[0] + 1*direction[1]
-            if psblIdx<64:
+            # print(psblIdx,prevIdx, abs(prevIdx%8-psblIdx%8))
+            if psblIdx>0 and psblIdx<64 and abs(prevIdx%8-psblIdx%8)<=1:
+                # print('uo')
+                   
                 if board_list[psblIdx] == opposite:
-                #    print(psblIdx, 'continue')
+                    #  print(psblIdx, 'continue')
                      continue
                 elif(board_list[psblIdx]==tokenToPlay):
-                #    print(psblIdx,'STOP')
+                    # print(psblIdx,'STOP')
                     moves.append(idx)
                     break
+                # elif(psblIdx%8==0):
+                #    break
+                # elif(psblIdx%8==7):
+                #    break
                 else:
+                    # print('STOP')
                     break
+
+            else:
+               break
    for move in moves:
       board_list[move] = '*'
         
@@ -69,12 +83,16 @@ if __name__ == '__main__':
     printBoard(boardUpdated)
     print('\n')
     print1DREP(board)
-    print(f"Possible moves for {tokenToPlay}:",*moves,'\n')
+    if moves:
+        print(f"Possible moves for {tokenToPlay}:",*set(moves),'\n')
+    else:
+       print("No moves possible")
 
 '''
 testing boards:
 
 '...................x.......xx......xo...........................'
+
 
 '''
 #Shaurya Jain, pd 3, 2025
