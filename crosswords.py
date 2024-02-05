b6904af752b6b7e1f572a4058b1edf6e693436f3
ShauryaJ1 <shaurya.s.jain@gmail.com>
2024-02-04 20:14:39 -0500

    need to add spacing

--- crosswords.py
+++ crosswords.py
@@ -4,6 +4,7 @@ import time
 global h
 global w
 global num_squares
+global symmetrical_lookup
 def file_to_lines():
     return open(args[2]).read().splitlines()
 h,w = int(args[0][:args[0].index('x')]) ,int(args[0][1+args[0].index('x'):])
@@ -13,6 +14,20 @@ seedStrings = ""
 if len(args)>3:
     seedStrings = args[3:]
 
+positions = [i for i in range(h*w)]
+p1 = 0
+p2 = h-1
+splits = [positions[i:i+w][::-1] for i in range(0,h*w,w)]
+while p1<=p2:
+        temp = splits[p1]
+        splits[p1] = splits[p2]
+        splits[p2] = temp
+        p1+=1
+        p2-=1
+reversed = []
+for s in splits:
+    reversed+=s
+symmetrical_lookup = {i:v for i,v in enumerate(reversed)}
 
 '''
 Place only symmetrically
@@ -41,6 +56,7 @@ def seedStringBreakdown(seedString):
         seedW, chars = split , '#'
     return orientation,seedH,seedW,chars
 def placeWord(xword_board,seedStringBreakdown):
+    minus_count = 0
     '''
     Accepts
         a crossword puzzle - string
@@ -60,16 +76,60 @@ def placeWord(xword_board,seedStringBreakdown):
         indices_to_change = [startIndex + i * w for i in range(len(charactersToPlace))]
     for char_idx,board_idx in enumerate(indices_to_change):
         placed[board_idx] = charactersToPlace[char_idx]
-    return ''.join(placed)
-def placeBlockingSquares(puzzle,num_squares):
+    if '#' in charactersToPlace:
+        block_indices = [i for i in range(len(charactersToPlace)) if charactersToPlace[i]=='#']
+        for i in block_indices:
+            if symmetrical_lookup[indices_to_change[i]] != indices_to_change[i]:
+                minus_count+=1
+            placed[symmetrical_lookup[indices_to_change[i]]] = '#'
+    
+    return ''.join(placed),minus_count
+def placeBlockingSquaresBasic(puzzle,num_squares):
     availables = [i for i in range(len(puzzle))if puzzle[i]=='-']
     puzzle_list= list(puzzle)
     for i in range(num_squares):
         puzzle_list[availables[i]] = '#'
+    return ''.join(puzzle_list) 
+def rotation(puzzle):
+    p1 = 0
+    p2 = h-1
+    splits = [puzzle[i:i+w][::-1] for i in range(0,h*w,w)]
+    while p1<=p2:
+        temp = splits[p1]
+        splits[p1] = splits[p2]
+        splits[p2] = temp
+        p1+=1
+        p2-=1
+    return ''.join(splits)
+def onlyBlocks(puzzle):
+    puzzle_list = list(puzzle)
+    for i in range(len(puzzle_list)):
+        if puzzle_list[i]!='#':
+            puzzle_list[i] = '-'
     return ''.join(puzzle_list)
-
+def isInvalid(puzzle):
+    return onlyBlocks(puzzle) != onlyBlocks(rotation(puzzle))
+def placeBlockingSquares(puzzle,num_blocks,availables):  
+    if isInvalid(puzzle):
+        return ''  
+    if num_blocks == 0:
+        return puzzle
+    if num_blocks==1:
+        for i in symmetrical_lookup:
+            if symmetrical_lookup[i] == i:
+                new_puzzle = list(puzzle)
+                new_puzzle[i] = '#'
+                return ''.join(new_puzzle)
+    for choice in availables:
+        sub_pzl = list(puzzle)
+        sub_pzl[choice] = '#'
+        sub_pzl[symmetrical_lookup[choice]] = '#'
+        bF = placeBlockingSquares(''.join(sub_pzl),num_blocks-2,availables-{choice,symmetrical_lookup[choice]})
+        if bF:
+            return bF
     
 
+    return ''
 def printPuzzle(puzzle,h,w,num_squares):
     '''
     Accepts 
@@ -94,8 +154,13 @@ if __name__ == "__main__":
     breakdowns = []
     for seedString in seedStrings:
         breakdowns.append(seedStringBreakdown(seedString))
-
+    print(breakdowns)
+    num_blocks = num_squares - sum(b[3].count('#') for b in breakdowns)
     for breakdown in breakdowns:
-        puzzle = placeWord(puzzle,breakdown)
-    print(placeBlockingSquares(printPuzzle(puzzle,h,w,int(num_squares)),num_squares))
+        puzzle,mc = placeWord(puzzle,breakdown)
+        num_blocks-=mc
+        
+    availables = {i for i in range(h*w) if puzzle[i]=='-' and puzzle[symmetrical_lookup[i]]=='-'}
+    final_puzzle = placeBlockingSquares(puzzle,num_blocks,availables)
+    print(printPuzzle(final_puzzle,h,w,0))
 #Shaurya Jain, pd 3, 2025
